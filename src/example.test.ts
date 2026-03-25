71b95be1b513230064e4f080919594a9da8978cb
Reece <business@reecebenson.me>
2026-03-25 20:41:12 +0000

    bug: upsert fails on not null constraint for id

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,34 +1,37 @@
-import 'reflect-metadata';
-import { Entity, PrimaryKey, Property, ReflectMetadataProvider } from '@mikro-orm/decorators/legacy';
-import { MikroORM } from '@mikro-orm/sqlite';
+import { MikroORM } from "@mikro-orm/sqlite";
+import { defineEntity, p } from "@mikro-orm/core";
+import { v4 } from "uuid";
 
-@Entity()
-class User {
+const UserSchema = defineEntity({
+  name: "User",
+  properties: {
+    id: p
+      .string()
+      .primary()
+      .index()
+      .onCreate(() => v4()),
+    createdAt: p.datetime().onCreate(() => new Date()),
+    updatedAt: p
+      .datetime()
+      .onCreate(() => new Date())
+      .onUpdate(() => new Date()),
+    email: p.string().unique().index(),
+    name: p.string(),
+  },
+});
 
-  @PrimaryKey()
-  id!: number;
-
-  @Property()
-  name: string;
-
-  @Property({ unique: true })
-  email: string;
-
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
-  }
-
+export class User extends UserSchema.class {
+  name: string = "";
 }
+UserSchema.setClass(User);
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
+    dbName: ":memory:",
     entities: [User],
-    metadataProvider: ReflectMetadataProvider,
-    debug: ['query', 'query-params'],
+    debug: ["query", "query-params"],
     allowGlobalContext: true, // only for testing
   });
   await orm.schema.refresh();
@@ -38,17 +41,21 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+test("basic CRUD example", async () => {
+  const createUser = new User();
+  createUser.email = "foo";
+  createUser.name = "test";
+  await orm.em.getRepository(User).upsert(createUser);
   await orm.em.flush();
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
+  const user = await orm.em.findOneOrFail(User, { email: "foo" });
+  expect(user.id).toBe(expect.any(String));
+
+  user.name = "Bar";
   orm.em.remove(user);
   await orm.em.flush();
 
-  const count = await orm.em.count(User, { email: 'foo' });
+  const count = await orm.em.count(User, { email: "foo" });
   expect(count).toBe(0);
 });
